ce884bb6d0257d94ac5fb5e6ef47662466f6e621
janagan-jgp <janagan4546@gmail.com>
2026-07-10 14:31:49 +0530

    Time: 1 ms (99.92%), Space: 43.4 MB (69.93%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        char[] charArrA = a.toCharArray();
+        char[] charArrB = b.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        int i = charArrA.length - 1, j = charArrB.length - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int aNum = i >= 0 ? charArrA[i] - '0' : 0;
+            int bNum = j >= 0 ? charArrB[j] - '0' : 0;
+
+            int sum = aNum + bNum + carry;
+            int digit = sum % 2;
+            carry = sum / 2;
+            i--;
+            j--;
+
+            sb.append((char)(digit + '0'));
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
